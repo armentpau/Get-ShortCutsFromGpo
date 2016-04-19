41737cca06d1d5d7fff9cdbd5985542ba6889977
Paul DeArment <paul.e.dearment.jr@gmail.com>
2016-04-19 10:36:46 -0500

    Initial commit
    Allows for pulling an GPO report of the local domain and exports this
    information to a csv file.
    
    Next steps - remote domain report

---
+++ Get-ShortCutsFromGpo.ps1
@@ -0,0 +1,29 @@
+﻿param
+(
+	[Parameter(Mandatory = $true,
+			   Position = 0)]
+	[string]$outFile
+)
+Import-Module GroupPolicy
+
+
+[xml]$data = Get-GPOReport -All -ReportType xml
+$holder = foreach ($gpo in $data.gpos.gpo)
+{
+	$Gponame = $gpo.name
+	foreach ($gpoShortcut in ($gpo.user.ExtensionData | Where-Object{ $_.name -like "Shortcuts" }).extension.shortcutsettings.shortcut)
+		{
+		$properties = [Ordered]@{
+			"ApplicationName" = $gpoShortcut.name;
+			"GPOName" = $Gponame;
+			"ShortcutPath" = $gpoShortcut.properties.shortcutpath;
+			"TargetType" = $gpoShortcut.properties.targettype;
+			"StartIn" = $gpoShortcut.properties.startin;
+			"TargetPath" = $gpoShortcut.properties.targetpath
+		}
+		$obj = New-Object -TypeName System.Management.Automation.PSObject -Property $properties
+		$obj
+	}
+}
+$holder | Export-Csv $outFile -NoTypeInformation
+
